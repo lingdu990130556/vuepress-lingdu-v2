4caa68f1f68cec9c30d93ffa30c5625831e0694a
〇° <990130556@qq.com>
2023-08-25 18:18:05 +0800

    更新百度统计数据

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -71,8 +71,8 @@ export default ({
                         path: "/",
                         title: '<button id="btnMove" style="background-color: transparent;border: none;cursor: pointer;">〇°</button>',
                         // content: "Notice Content",// 内容
-                        content: "<iframe frameborder=\"no\" border=\"0\" marginwidth=\"0\" marginheight=\"0\" width=100% height=500 src=\"https://lingdu.love/yinyue/\"></iframe>",
-                        // content: "<iframe frameborder=\"no\" border=\"0\" marginwidth=\"0\" marginheight=\"0\" width=100% height=500 src=\"/vuepress-lingdu-v2/html/yinyue/bofang.html\"></iframe>",
+                        // content: "<iframe frameborder=\"no\" border=\"0\" marginwidth=\"0\" marginheight=\"0\" width=100% height=500 src=\"https://lingdu.love/yinyue/\"></iframe>",
+                        content: "<iframe frameborder=\"no\" border=\"0\" marginwidth=\"0\" marginheight=\"0\" width=100% height=500 src=\"/vuepress-lingdu-v2/html/yinyue/bofang.html\"></iframe>",
                         // 全屏显示
                         fullscreen: false,
                         // 需要确认才关闭否则延时关闭
